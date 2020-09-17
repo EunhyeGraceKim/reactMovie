1ff8d3c7de67991de5245bfdd9ae3304054c0865
Eunhye Kim <Eunhye@Chanbinui-MacBook-Pro.local>
2020-09-17 15:33:08 +0900

    react study 20200917

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Potato from './Potato';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br>Hello Grace</br>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrerß"
-        >
-          Learn React !!
-        </a>
-      </header>
-    </div>
+  <div className="App" >
+    <h1>Hello</h1>
+    <Potato />
+  </div>
   );
 }
 
