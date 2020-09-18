a133ae4d87980b429d6dfe6427347e7fa9b1562b
Eunhye Kim <Eunhye@Chanbinui-MacBook-Pro.local>
2020-09-18 09:46:29 +0900

    react movie2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({name, picture}){
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name : "kimchi",
+    image : "https://static.production.thenounproject.com/img/favicons/apple-touch-icon.7fb1143e988e.png"
+  },
+  {
+    name : "veg",
+    image : "https://static.production.thenounproject.com/img/favicons/favicon-32x32.015f779a87e7.png"
+  },
+  {
+    name : "anyting",
+    image : "https://static.production.thenounproject.com/img/favicons/favicon-16x16.0b6183ac3673.png"
+  }
+]
 
 function App() {
-  return (
-  <div className="App" >
-    <h1>Hello</h1>
-    <Potato />
-  </div>
+  return ( 
+    <div>
+      {foodILike.map(dish => (
+      <Food name={dish.name} picture={dish.image} />
+      ))}
+    </div>
   );
 }
 
