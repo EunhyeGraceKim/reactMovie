787996aaa8d18bdf8b4c5b583db06fbfa0ffb425
Eunhye Kim <Eunhye@Chanbinui-MacBook-Pro.local>
2020-09-20 16:37:25 +0900

    add axios_0920

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component{
+  
   state = {
-    count :0
+    isLoading : true,
+    movies : []
+  };
+  getMovies = async() =>{
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
   }
-  add= () => {
-    this.setState(current => ({count : current.count + 1}));
-  };
-  minus =() => {
-    this.setState(current => ({count : current.count - 1}));
-  };
+  componentDidMount(){
+   this.getMovies();
+  }
   render(){
-    return (
-      <div>
-        <h1>The number is : {this.state.count} </h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
-    );
-  }
+    const {isLoading} = this.state;
+    return <div> {isLoading ? "Loading..." : "We are ready"}</div>;
+    
+  } 
 }
 export default App;
